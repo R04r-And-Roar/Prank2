1a895e29d0431079a0dedc14c0865e7827625fdb
R04r-And-Roar <78694745+R04r-And-Roar@users.noreply.github.com>
2021-02-07 23:13:11 +0700

    Add files via upload
    Sorry yang pertama ada yang salah

---
+++ haha.sh
@@ -0,0 +1,5 @@
+echo  "HAHAHAHHAHAHAHAH KENA PRANK"
+sleep  3
+echo  "mangkanya punya tujuan hack fb tu yang baik bukan buat sembarangan contohnya hanya buat pengetahuan...jadi gitu ya :D"
+       
+      
---
+++ test.sh
@@ -0,0 +1,15 @@
+echo -e "\e[31mHack Akun Facebook?\e[0m";
+echo -e "\e[33mPencet 1\e[0m";
+echo -e " 1.HACK FB"
+echo -e "2.keluar"
+read -p "pilih (1/2): " pil
+if [ $pil = "1" ]
+then
+    bash haha.sh
+elif [ $pil = "2" ]
+then
+    echo -p " Bye-Bye"
+else			
+    echo -p "Selamat Tinggal"
+fi
+			
